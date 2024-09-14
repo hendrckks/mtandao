7a750b8ab971d82ed3e52ec79de0458ab813fbe6
Samuel tetenga <126316270+hendrckks@users.noreply.github.com>
2024-09-14 17:30:18 +0300

    contact icon type error

--- src/components/icons/ContactSvg.tsx
+++ src/components/icons/ContactSvg.tsx
@@ -1,22 +1,28 @@
-import React from 'react';
-
-interface SVGProps extends React.SVGProps<SVGSVGElement> {}
-
-const ContactSvg: React.FC<SVGProps> = () => {
-  return (
-    <div>
-      <svg
-        style="width:100%;height:100%"
-        viewBox="0 0 716 901"
-        preserveAspectRatio="none"
-        width="100%"
-        height="100%"
-      >
-        <use href="#svg1058065819_13005"></use>
-      </svg>
-      ;
-    </div>
-  );
-};
-
-export default ContactSvg;
+[
+  {
+    resource:
+      '/c:/Users/Sam/Desktop/mtandao/src/components/icons/ContactSvg.tsx',
+    owner: 'typescript',
+    code: '2559',
+    severity: 8,
+    message:
+      "Type 'string' has no properties in common with type 'Properties<string | number, string & {}>'.",
+    source: 'ts',
+    startLineNumber: 9,
+    startColumn: 9,
+    endLineNumber: 9,
+    endColumn: 14,
+    relatedInformation: [
+      {
+        startLineNumber: 3656,
+        startColumn: 9,
+        endLineNumber: 3656,
+        endColumn: 14,
+        message:
+          "The expected type comes from property 'style' which is declared here on type 'SVGProps<SVGSVGElement>'",
+        resource:
+          '/c:/Users/Sam/Desktop/mtandao/node_modules/@types/react/index.d.ts',
+      },
+    ],
+  },
+];
